44442743a0084c637eac9d880b720661bad660c8
Clayton Rieck <cjrieck123@gmail.com>
2013-11-03 00:13:37 -0400

    Updated generate.py, can create functions with arguments and return values and writes the main function into the file

--- generate.py
+++ generate.py
@@ -16,17 +16,44 @@ def main():
 		# fileName.close()
 
 	if args.functions:
+
 		for function in args.functions:
-			argumentString = raw_input("Enter argument(s) for "+function)
-			if argumentString.find(',') != -1:
+
+			argumentString = raw_input("Enter argument(s) for "+function+': ')
+
+			if argumentString == '':
+				argumentList = []
+			elif argumentString.find(',') != -1:
 				argumentList = argumentString.split(',')
+				print argumentList
 			else:
 				argumentList = argumentString.split(' ')
+			# argumentList = str(tuple(argumentList))
+			argumentString = ','.join(argumentList)
 
-			argumentList = str(tuple(argumentList))
+			fileName.write('def '+function+'('+argumentString+')'+':')
 
+			returnString = raw_input("Enter return value(s) for "+function+': ')
 			
+			if returnString == '':
+				returnValueList = []
+			elif returnString.find(',') != -1:
+				returnValueList = returnString.split(',')
+			else:
+				returnValueList = returnString.split(' ')
+			
+			if returnValueList == []:
+				fileName.write('\n\tpass # remove this and replace with the function body')
+			else:
+				returnString = ','.join(returnValueList)
+				fileName.write('\n\treturn '+returnString)
 
+			fileName.write('\n\n')
+
+		fileName.write("def main():\n\tpass")
+		fileName.write("\n\nif __name__ == '__main__':\n\tmain()")
+
+		fileName.close()
 
 if __name__ == '__main__':
 	main()
