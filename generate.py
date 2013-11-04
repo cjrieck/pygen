dda15c0a6dbc55f9c9e335d646b78078a2e5f1e9
Clayton Rieck <cjrieck123@gmail.com>
2013-11-04 12:09:02 -0500

    Added base functionality for classes generation and consolidated repeated code into their own functions

--- generate.py
+++ generate.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python
-
 import argparse
+
+def check_input(inputString):
+
+	if inputString == '':
+		inputList = []
+		
+	elif inputString.find(',') != -1:
+		inputList = inputString.split(',')
+	else:
+		inputList = inputString.split(' ')
+
+	return inputList
+
+
+def create_functions(fileName, function, delimiter=''):
+
+	argumentString = raw_input("Enter argument(s) for "+function+': ')
+
+	argumentList = check_input(argumentString)
+
+	argumentString = ','.join(argumentList)
+	
+	fileName.write('def '+function+'('+argumentString+')'+':')
+
+	returnString = raw_input("Enter return value(s) for "+function+': ')
+	
+	returnValueList = check_input(returnString)
+
+	if returnValueList == []:
+		fileName.write('\n\tpass # remove this and replace with the function body')
+	else:
+		returnString = ','.join(returnValueList)
+		fileName.write('\n\t'+delimiter+'return '+returnString)
+
+	fileName.write('\n\n')
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('file', help='name of the file you want to create')
-	parser.add_argument('-i', '--import', nargs='+', help='modules you want to import')
+	parser.add_argument('-i', '--imports', nargs='+', help='modules you want to import')
 	parser.add_argument('-c', '--classes', nargs='+', help='names of classes you want to create')
 	parser.add_argument('-f', '--functions', nargs='+', help='names of functions you want to create')
 
@@ -17,40 +51,49 @@ def main():
 		fileName = open(args.file+'.py', 'w')
 		# fileName.close()
 
+		if args.imports:
+			pass
+
+		if args.classes:
+
+			for className in args.classes:
+				fileName.write('class '+className+':')
+
+				dataString = raw_input('Enter data to be stored in the class, '+className+': ')
+
+				dataList = check_input(dataString)
+
+				argumentDataList = []
+
+				for arg in dataList:
+					newArg = arg+'=None'
+					argumentDataList.append(newArg)
+					
+
+				dataString = ','.join(argumentDataList)
+				fileName.write('\n\tdef __init__('+dataString+'):')
+
+				for arg in argumentDataList:
+					fileName.write('\n\t\tself.'+arg.replace(' ', '').replace('=None', '')+' = '+arg.replace('=None', '').replace(' ', ''))
+
+				fileName.write('\n\n')
+
+				methodString = raw_input('Enter the methods of class, '+className+': ')
+
+				# write methods to the class body
+
+				methodList = check_input(methodString)
+
+				for method in methodList:
+					fileName.write('\t')
+					create_functions(fileName, method, '\t')
+
+
 		if args.functions:
 
 			for function in args.functions:
 
-				argumentString = raw_input("Enter argument(s) for "+function+': ')
-
-				if argumentString == '':
-					argumentList = []
-				elif argumentString.find(',') != -1:
-					argumentList = argumentString.split(',')
-					# print argumentList
-				else:
-					argumentList = argumentString.split(' ')
-				# argumentList = str(tuple(argumentList))
-				argumentString = ','.join(argumentList)
-
-				fileName.write('def '+function+'('+argumentString+')'+':')
-
-				returnString = raw_input("Enter return value(s) for "+function+': ')
-				
-				if returnString == '':
-					returnValueList = []
-				elif returnString.find(',') != -1:
-					returnValueList = returnString.split(',')
-				else:
-					returnValueList = returnString.split(' ')
-				
-				if returnValueList == []:
-					fileName.write('\n\tpass # remove this and replace with the function body')
-				else:
-					returnString = ','.join(returnValueList)
-					fileName.write('\n\treturn '+returnString)
-
-				fileName.write('\n\n')
+				create_functions(fileName, function)
 
 			fileName.write("def main():\n\tpass")
 			fileName.write("\n\nif __name__ == '__main__':\n\tmain()")
@@ -59,7 +102,7 @@ def main():
 			fileName.write("def main():\n\tpass")
 			fileName.write("\n\nif __name__ == '__main__':\n\tmain()")
 		
-		fileName.close()
+	fileName.close()
 
 if __name__ == '__main__':
 	main()
